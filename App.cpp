695b7c998602edbebcf23e61b6681b71f86753d6
LidorY <lidor323@gmail.com>
2021-02-14 19:41:41 +0200

    Legal notice addition in the main app source dfile bottom

--- App.cpp
+++ App.cpp
@@ -1,3 +1,5 @@
+
+
 #include "pch.h"
 
 // Standard library includes
@@ -167,3 +169,36 @@ int __stdcall wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
 {
     CoreApplication::Run(make<App>());
 }
+
+
+
+
+/*
+* This code, and a great portion of the DirectX related code is inspired by,
+* and contains code snippets from Microsoft's DirectX tutorials and samples.
+* The main related repository may be found here:
+* https://github.com/microsoft/Windows-universal-samples/blob/master/Samples
+*
+* Copyright and Legal notice:
+* ----------------------------------------------------------------------------------
+* Copyright (c) Microsoft Corporation
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+*  of this software and associated documentation files (the "Software"), to deal
+*  in the Software without restriction, including without limitation the rights
+*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*  copies of the Software, and to permit persons to whom the Software is
+*  furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+*  all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+*  THE SOFTWARE.
+* ----------------------------------------------------------------------------------
+*/
